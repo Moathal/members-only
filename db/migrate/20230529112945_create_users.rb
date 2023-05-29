c897190b3fe85188722e2e0ad05ebb2ddd8220f9
Moathal <moathal.s.kachi@gmail.com>
2023-05-29 14:29:58 +0300

    Create user model

---
+++ db/migrate/20230529112945_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.date :birthday
+      t.text :bio
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
