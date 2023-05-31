ead214ff87437abc89b6e4c28b1121d689d92d4b
Moathal <moathal.s.kachi@gmail.com>
2023-05-31 18:19:23 +0300

    Add the other parameters to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :birthday, :bio,])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :birthday, :bio,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :birthday, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :birthday, :bio])
   end
 end
